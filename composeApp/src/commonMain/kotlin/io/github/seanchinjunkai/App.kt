abd956518229eb970d60dd776bb0481d6b239608
SeanChinJunKai <e0774037@u.nus.edu>
2025-04-07 18:44:55 +0800

    Complete sample app to showcase vertexgsoc kmp library

--- composeApp/src/commonMain/kotlin/io/github/seanchinjunkai/App.kt
+++ composeApp/src/commonMain/kotlin/io/github/seanchinjunkai/App.kt
@@ -1,37 +1,75 @@
 package io.github.seanchinjunkai
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Button
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.TextButton
+import androidx.compose.material.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import org.jetbrains.compose.resources.painterResource
+import androidx.compose.ui.unit.dp
+import io.github.seanchinjunkai.vertexgsoc.GenerativeModel
+import io.github.seanchinjunkai.vertexgsoc.RequestOptions
+import kotlinx.coroutines.launch
 import org.jetbrains.compose.ui.tooling.preview.Preview
-
-import vertexsample.composeapp.generated.resources.Res
-import vertexsample.composeapp.generated.resources.compose_multiplatform
 
 @Composable
 @Preview
 fun App() {
+    val api = remember { GenerativeModel(key = "API_KEY", model = "gemini-1.5-flash-002", RequestOptions(projectId = "gemini-kmp"))}
+
+    val coroutineScope = rememberCoroutineScope()
+    var prompt by remember { mutableStateOf("Summarize the benefits of Kotlin Multiplatform") }
+    var content by remember { mutableStateOf("") }
+    var showProgress by remember { mutableStateOf(false) }
+
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
+        Column(Modifier.verticalScroll(rememberScrollState())
+            .fillMaxWidth().padding(16.dp)
+        ) {
+            Row {
+                TextField(value = prompt,
+                    onValueChange = { prompt = it },
+                    modifier = Modifier.weight(7f)
+                )
+                TextButton(onClick = {
+                    if (prompt.isNotBlank()) {
+                        coroutineScope.launch {
+                            showProgress = true
+                            content = generateContent(api, prompt)
+                            showProgress = false
+                        }
+                    }
+                },
+                    modifier = Modifier.weight(3f)
+                        .padding(all = 4.dp)
+                        .align(Alignment.CenterVertically)
+                ) {
+                    Text("Submit")
                 }
+            }
+
+            Spacer(Modifier.height(16.dp))
+            if (showProgress) {
+                CircularProgressIndicator()
+            } else {
+                Text(content)
             }
         }
     }
+}
+
+suspend fun generateContent(api: GenerativeModel, prompt: String): String {
+    println("prompt = $prompt")
+    val result = api.generateContent(prompt)
+    return result.response?.candidates?.get(0)?.content?.parts?.get(0)?.text ?: "NO RESULTS"
 }
